88ae65c2e7becd433a716b33766d36b110b5699b
gene9831 <pacify.98@gmail.com>
2020-11-19 01:39:32 +0800

    github icon fixed

--- src/components/ForkMe.js
+++ src/components/ForkMe.js
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme) => {
           ? theme.palette.success.main
           : theme.palette.info.light,
       color: '#fff',
-      position: 'absolute',
+      position: 'fixed',
       top: 0,
       border: 0,
       right: 0,
