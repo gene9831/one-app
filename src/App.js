405f512a6385bd15095932086419357cfcaa2c0c
gene9831 <pacify.98@gmail.com>
2020-11-18 13:23:09 +0800

    基于路由的代码分割

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import Admin from './components/Admin';
+import React, { lazy, Suspense } from 'react';
 import Theme from './components/Theme';
 import GlobalSnackbar from './components/GlobalSnackbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NoRoute from './components/NoRoute';
-import ItemList from './components/ItemList';
+
+const ItemList = lazy(() => import('./components/ItemList'));
+const Admin = lazy(() => import('./components/Admin'));
 
 export default function App() {
   return (
     <Theme>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <ItemList />
-          </Route>
-          <Route path="/admin">
-            <Admin />
-          </Route>
-          <Route>
-            <NoRoute />
-          </Route>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/">
+              <ItemList />
+            </Route>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route>
+              <NoRoute />
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
       <GlobalSnackbar />
     </Theme>
