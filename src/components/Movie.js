40e52c6b412558e4d8dd21f42f3a7efb6a93e94c
gene9831 <pacify.98@gmail.com>
2020-11-23 04:11:14 +0800

    有资源啦

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,29 @@
-import { Chip, makeStyles, Paper, Typography } from '@material-ui/core';
+import {
+  Chip,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import apiRequest from '../api';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { withResizeDetector } from 'react-resize-detector';
-import { detectMob } from '../utils';
+import { bTokmg, detectMob } from '../utils';
+import FolderOpenIcon from '@material-ui/icons/FolderOpen';
+import InsertDriveFileOutlinedIcon from '@material-ui/icons/InsertDriveFileOutlined';
+import { PlayBoxOutline } from './Icons';
+import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import SubtitlesOutlinedIcon from '@material-ui/icons/SubtitlesOutlined';
+import ComponentShell from './ComponentShell';
+import DialogWithFIle from './DialogWithFIle';
 
 const tmdbImageUrl = 'https://image.tmdb.org/t/p';
 
@@ -58,6 +76,16 @@ const useStyles = makeStyles((theme) => ({
   overview: {
     padding: theme.spacing(1, 0),
   },
+  flex: {
+    display: 'flex',
+  },
+  itemIcon: {
+    marginRight: theme.spacing(1),
+  },
+  ellipsis: {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
 }));
 
 let AdaptivePaper = ({ width, height, targetRef, children, ...others }) => {
@@ -80,9 +108,25 @@ AdaptivePaper.propTypes = {
 
 AdaptivePaper = withResizeDetector(AdaptivePaper);
 
+const getItemIcon = (item) => {
+  if (item.folder) return FolderOpenIcon;
+  const mimeType = item.file.mimeType;
+  if (mimeType.startsWith('video')) return PlayBoxOutline;
+  if (mimeType.startsWith('text')) return DescriptionOutlinedIcon;
+  if (
+    item.name.endsWith('srt') ||
+    item.name.endsWith('ssa') ||
+    item.name.endsWith('ass')
+  )
+    return SubtitlesOutlinedIcon;
+  return InsertDriveFileOutlinedIcon;
+};
+
 const Movie = (props) => {
   const { movieId } = useParams();
   const [movieData, setMovieData] = useState(props.movie);
+  const [resourceItems, setResourceItems] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const backdropsUrl = React.useMemo(() => {
     if (!movieData) return '';
@@ -103,55 +147,138 @@ const Movie = (props) => {
     fetchData();
   }, [movieId]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      let res = await apiRequest('TMDb.getItemsByMovieId', {
+        params: [parseInt(movieId)],
+      });
+      setResourceItems(res.data.result);
+    };
+    fetchData();
+  }, [movieId]);
+
   return (
-    <AdaptivePaper className={classes.paper}>
-      <div className={classes.poster}>
+    <>
+      <AdaptivePaper className={classes.paper}>
+        <div className={classes.poster}>
+          {valid ? (
+            <img
+              src={`${tmdbImageUrl}/w500${movieData.images.posters[0].file_path}`}
+              alt="poster"
+            />
+          ) : null}
+        </div>
         {valid ? (
-          <img
-            src={`${tmdbImageUrl}/w500${movieData.images.posters[0].file_path}`}
-            alt="poster"
-          />
-        ) : null}
-      </div>
-      {valid ? (
-        <div style={{ marginLeft: 24 }}>
-          <div style={{ display: 'flex', alignItems: 'flex-end' }}>
-            <Typography variant="h4" className={classes.textPrimary}>
-              {movieData.title}
-            </Typography>
-            <Typography
-              className={clsx(classes.textSecondary, classes.releaseDate)}
-            >
-              ({movieData.release_date.slice(0, 4)})
-            </Typography>
-          </div>
-          {movieData.genres.map((item) => (
+          <div style={{ marginLeft: 24 }}>
+            <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Typography variant="h4" className={classes.textPrimary}>
+                {movieData.title}
+              </Typography>
+              <Typography
+                className={clsx(classes.textSecondary, classes.releaseDate)}
+              >
+                ({movieData.release_date.slice(0, 4)})
+              </Typography>
+            </div>
+            {movieData.genres.map((item) => (
+              <Chip
+                key={item.name}
+                variant="outlined"
+                size="small"
+                label={item.name}
+                className={clsx(classes.textSecondary, classes.chipBorder)}
+              />
+            ))}
             <Chip
-              key={item.name}
               variant="outlined"
               size="small"
-              label={item.name}
+              label={`${movieData.runtime}分钟`}
               className={clsx(classes.textSecondary, classes.chipBorder)}
             />
-          ))}
-          <Chip
-            variant="outlined"
-            size="small"
-            label={`${movieData.runtime}分钟`}
-            className={clsx(classes.textSecondary, classes.chipBorder)}
-          />
-          <Typography
-            variant="h6"
-            className={clsx(classes.textPrimary, classes.overview)}
+            <Typography
+              variant="h6"
+              className={clsx(classes.textPrimary, classes.overview)}
+            >
+              剧情简介
+            </Typography>
+            <Typography variant="body1" className={classes.textPrimary}>
+              {movieData.overview}
+            </Typography>
+          </div>
+        ) : null}
+      </AdaptivePaper>
+      <Paper style={{ marginTop: 24, padding: 16 }}>
+        <Typography variant="h5">资源</Typography>
+        <TableContainer>
+          <Table
+            style={{
+              whiteSpace: 'nowrap',
+            }}
           >
-            剧情简介
-          </Typography>
-          <Typography variant="body1" className={classes.textPrimary}>
-            {movieData.overview}
-          </Typography>
-        </div>
-      ) : null}
-    </AdaptivePaper>
+            <TableHead>
+              <TableRow>
+                {['名称', '修改日期', '类型', '大小'].map((item, index) => (
+                  <TableCell key={index}>
+                    <Typography>{item}</Typography>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {resourceItems.map((row, index) => (
+                <TableRow
+                  key={index}
+                  hover
+                  onClick={() => setSelectedFile(row)}
+                >
+                  <TableCell className={classes.flex}>
+                    <ComponentShell
+                      Component={getItemIcon(row)}
+                      Props={{ className: classes.itemIcon }}
+                    />
+                    <Typography className={classes.ellipsis}>
+                      {row.name}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography className={classes.ellipsis}>
+                      {new Date(row.lastModifiedDateTime).toLocaleString([], {
+                        year: 'numeric',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour12: false,
+                      })}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography className={classes.ellipsis}>
+                      {(() => {
+                        if (row.folder) return '文件夹';
+                        const idx = row.name.lastIndexOf('.');
+                        if (idx < 0) return '.file';
+                        return row.name.slice(idx + 1).toUpperCase();
+                      })()}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography className={classes.ellipsis}>
+                      {row.folder
+                        ? `${row.folder.childCount}项`
+                        : bTokmg(row.size)}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <DialogWithFIle
+          open={Boolean(selectedFile)}
+          file={selectedFile || {}}
+          onClose={() => setSelectedFile(null)}
+        />
+      </Paper>
+    </>
   );
 };
 
