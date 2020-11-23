b90af5ada49128b5661c085b38a8f5438a6aef7b
gene9831 <pacify.98@gmail.com>
2020-11-24 02:07:52 +0800

    分离组件

--- src/components/MyAppBar.js
+++ src/components/MyAppBar.js
@@ -1,88 +1,25 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import clsx from 'clsx';
 import { connect } from 'react-redux';
 
-const useStyles = makeStyles((theme) => {
-  const mediaUpSm = theme.breakpoints.up('sm');
-  const mediaUpMd = theme.breakpoints.up('md');
-
-  return {
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      [mediaUpMd]: {
-        marginLeft: (props) => props.shiftWidth,
-        width: (props) => `calc(100% - ${props.shiftWidth}px)`,
-      },
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: theme.spacing(0.5),
-      [mediaUpSm]: {
-        marginRight: theme.spacing(2),
-      },
-      [mediaUpMd]: {
-        marginRight: theme.spacing(4),
-      },
-    },
-    menuButtonHide: {
-      [mediaUpMd]: {
-        display: 'none',
-      },
-    },
-    title: {
-      flexGrow: 1,
-    },
-  };
-});
+const useStyles = makeStyles(() => ({
+  div: {
+    flexGrow: 1,
+  },
+}));
 
 let MyAppBar = (props) => {
-  const { shift, title, onClickMenu, endComponents, color, startIcon } = props;
-  const classes = useStyles(props);
+  const { startComponents, endComponents, color, classes } = props;
+  const styles = useStyles();
 
   return (
-    <AppBar
-      position="fixed"
-      className={clsx(classes.appBar, {
-        [classes.appBarShift]: shift,
-      })}
-      color={color}
-    >
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          onClick={onClickMenu}
-          className={clsx(classes.menuButton, {
-            [classes.menuButtonHide]: shift,
-          })}
-        >
-          {startIcon}
-        </IconButton>
-        <Typography
-          component="h1"
-          variant="h6"
-          color="inherit"
-          noWrap
-          className={classes.title}
-        >
-          {title}
-        </Typography>
+    <AppBar position="fixed" className={classes.appBar} color={color}>
+      <Toolbar className={classes.toolbar}>
+        {startComponents}
+        <div className={styles.div}></div>
         {endComponents}
       </Toolbar>
     </AppBar>
@@ -90,18 +27,14 @@ let MyAppBar = (props) => {
 };
 
 MyAppBar.propTypes = {
-  shift: PropTypes.bool,
-  shiftWidth: PropTypes.number,
-  title: PropTypes.string,
-  onClickMenu: PropTypes.func,
-  endComponents: PropTypes.any,
+  startComponents: PropTypes.node,
+  endComponents: PropTypes.node,
   color: PropTypes.string,
-  startIcon: PropTypes.node,
+  classes: PropTypes.object,
 };
 
 MyAppBar.defaultProps = {
-  shiftWidth: 240,
-  startIcon: <MenuIcon />,
+  classes: {},
 };
 
 MyAppBar = connect((state) => ({
---
+++ src/components/MyAppBarWithDrawer.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import MyAppBar from './MyAppBar';
+
+const useStyles = makeStyles((theme) => {
+  const mediaUpSm = theme.breakpoints.up('sm');
+  const mediaUpMd = theme.breakpoints.up('md');
+
+  return {
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      [mediaUpMd]: {
+        marginLeft: (props) => props.shiftWidth,
+        width: (props) => `calc(100% - ${props.shiftWidth}px)`,
+      },
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(0.5),
+      [mediaUpSm]: {
+        marginRight: theme.spacing(2),
+      },
+      [mediaUpMd]: {
+        marginRight: theme.spacing(4),
+      },
+    },
+    menuButtonHide: {
+      [mediaUpMd]: {
+        display: 'none',
+      },
+    },
+  };
+});
+
+let MyAppBarWithDrawer = (props) => {
+  const { shift, title, onClickMenu, endComponents, startIcon } = props;
+  const classes = useStyles(props);
+
+  return (
+    <MyAppBar
+      classes={{
+        appBar: clsx(classes.appBar, {
+          [classes.appBarShift]: shift,
+        }),
+      }}
+      startComponents={[
+        <IconButton
+          key="1"
+          edge="start"
+          color="inherit"
+          onClick={onClickMenu}
+          className={clsx(classes.menuButton, {
+            [classes.menuButtonHide]: shift,
+          })}
+        >
+          {startIcon}
+        </IconButton>,
+        <Typography key="2" component="h1" variant="h6" color="inherit" noWrap>
+          {title}
+        </Typography>,
+      ]}
+      endComponents={endComponents}
+    />
+  );
+};
+
+MyAppBarWithDrawer.propTypes = {
+  shift: PropTypes.bool,
+  shiftWidth: PropTypes.number,
+  title: PropTypes.string,
+  onClickMenu: PropTypes.func,
+  endComponents: PropTypes.any,
+  startIcon: PropTypes.node,
+};
+
+MyAppBarWithDrawer.defaultProps = {
+  shiftWidth: 240,
+  startIcon: <MenuIcon />,
+};
+
+export default MyAppBarWithDrawer;
